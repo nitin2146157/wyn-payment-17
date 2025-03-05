859807676c2631bf42c5462e1a5ab153892f2044
Nitin Patil <2146157@cognizant.com>
2025-03-05 13:30:14 +0530

    base project

---
+++ src/main/java/com/wyn/payment/WynPayment17Application.java
@@ -0,0 +1,13 @@
+package com.wyn.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WynPayment17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WynPayment17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyn/payment/WynPayment17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyn.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WynPayment17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
